ec62deb901de474b0b0f9e948b487ce72df13ed0
NadiaA14 <nadia.almurshedy@hotmail.com>
2022-08-16 04:06:54 +0100

    css

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import App from '../App';
+import styled from 'styled-components';
 
 
 const CardDetail = ({card}) => {
+    
+    const Card = styled.div`
+    width: 250px;
+    height: 450px;
+    background-color: #ebe8e1;
+    border-radius: 10px;
+    border: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #fe4655;
+    line-height: 2;
+    padding: 5px;
+    `
 
 return (
-    <div className='Card'>
+    <Card>
     <h4>{card.name}</h4>
     <img src={card.picture} alt="characterpicture" width="200px" ></img>
-    <h5>Attack: {card.attack}</h5>
-    <h5>Control: {card.control}</h5>
-    <h5>Defence: {card.defence}</h5>
-    <h5>Frags: {card.frags}</h5>
-    <h5>Lurk: {card.lurk}</h5>
-    </div>
+    <b>Attack: {card.attack}</b>
+    <b>Control: {card.control}</b>
+    <b>Defence: {card.defence}</b>
+    <b>Frags: {card.frags}</b>
+    <b>Lurk: {card.lurk}</b>
+    </Card>
     )
 }
 
